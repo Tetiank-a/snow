eed07c290379e062b31effa37dbefc7bbae1a97b
Tetiana Rakova <tatyrakova2002@gmail.com>
2021-11-09 08:09:27 +0200

    added levels table

--- main.py
+++ main.py
@@ -14,22 +14,25 @@ db = mongodb_client.db
 def hello():
     return "Hello World!"
 
+# USERS
 @app.route('/users', methods=['POST'])
 def create_user():
     # Receiving Data
     username = request.json['username']
     email = request.json['email']
+    level_id = request.json['level_id']
     password = request.json['password']
 
-    if username and email and password:
+    if username and email and level_id and password:
         hashed_password = generate_password_hash(password)
         id = db.users.insert(
-            {'username': username, 'email': email, 'password': hashed_password})
+            {'username': username, 'email': email, 'level_id': level_id, 'password': hashed_password})
         response = jsonify({
             '_id': str(id),
             'username': username,
             'password': password,
-            'email': email
+            'email': email,
+            'level_id': level_id
         })
         response.status_code = 201
         return response
@@ -64,12 +67,67 @@ def delete_user(id):
 def update_user(_id):
     username = request.json['username']
     email = request.json['email']
+    level_id = request.json['level_id']
     password = request.json['password']
     if username and email and password and _id:
         hashed_password = generate_password_hash(password)
         db.users.update_one(
-            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'username': username, 'email': email, 'password': hashed_password}})
+            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'username': username, 'email': email, 'level_id': level_id, 'password': hashed_password}})
         response = jsonify({'message': 'User' + _id + 'Updated Successfuly'})
+        response.status_code = 200
+        return response
+    else:
+      return not_found()
+
+# LEVELS
+@app.route('/levels', methods=['POST'])
+def create_level():
+    # Receiving Data
+    name = request.json['name']
+
+    if name:
+        id = db.levels.insert(
+            {'name': name})
+        response = jsonify({
+            '_id': str(id),
+            'name': name
+        })
+        response.status_code = 201
+        return response
+    else:
+        return not_found()
+
+
+@app.route('/levels', methods=['GET'])
+def get_levels():
+    levels = db.levels.find()
+    response = json_util.dumps(levels)
+    return Response(response, mimetype="application/json")
+
+
+@app.route('/levels/<id>', methods=['GET'])
+def get_level(id):
+    print(id)
+    level = db.levels.find_one({'_id': ObjectId(id), })
+    response = json_util.dumps(level)
+    return Response(response, mimetype="application/json")
+
+
+@app.route('/levels/<id>', methods=['DELETE'])
+def delete_level(id):
+    db.levels.delete_one({'_id': ObjectId(id)})
+    response = jsonify({'message': 'Level' + id + ' Deleted Successfully'})
+    response.status_code = 200
+    return response
+
+
+@app.route('/levels/<_id>', methods=['PUT'])
+def update_level(_id):
+    name = request.json['name']
+    if name and _id:
+        db.levels.update_one(
+            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'name': name}})
+        response = jsonify({'message': 'Level' + _id + 'Updated Successfuly'})
         response.status_code = 200
         return response
     else:
